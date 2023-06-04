dfe47c6549c9b4a22881caf373086cde19a6386b
ATtami YYNa <tami19996@gmail.com>
2023-06-04 12:15:27 +0900

    update volume mastering

--- synthesizer.py
+++ synthesizer.py
@@ -9,8 +9,8 @@ RATE= 44100
 bufsize = 256
 pitch = []
 
-key_name = ["z","s","x","d","c","v","g","b","h","n","j","m",",","l",".",";","/","q","2","w","3","e","r","5","t","6","y","7","u","i","9","o","0","p","["]
-key_diff = [-9 ,-8 ,-7 ,-6 ,-5 ,-4 ,-3 ,-2 ,-1 , 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10 , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+key_name = ["z","s","x","d","c","v","g","b","h","n","j","m",",","l",".",";","/","q","2","w","3","e","r","5","t","6","y","7","u","i","9","o","0","p","[","=","]", "\\"]
+key_diff = [-9 ,-8 ,-7 ,-6 ,-5 ,-4 ,-3 ,-2 ,-1 , 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10 , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ,21 ,22, 24]
 key_frequency = {}
 for key,diff in zip(key_name,key_diff):
   key_frequency[key] = 440 * math.pow(2,diff * (1/12.0))
@@ -20,17 +20,13 @@ pos = 0
 def synthesize():
     global pos
     wave = np.zeros(bufsize)
-    count = 0
     for i in pitch:
-        count += 1
         t = i * (x+pos) / RATE
         t = t - np.trunc(t)
-        if count % 2 == 0:
-            wave += np.sin(2.0*np.pi*t)
-        else:
-            wave -= np.sin(2.0*np.pi*t)
-        wave -= np.sin(2.001*np.pi*t*t) * 0.52
-        wave += np.sin(2.03*np.pi*t*t*t) * 0.44
+        
+        wave += np.sin(2.0*np.pi*t)
+        wave -= np.sin(2.0*np.pi*t*t)
+        wave += np.sin(2.0*np.pi*t*t*t)
         wave -= np.sin(15*np.pi*t) * 0.25
         wave += np.sin(8.5*np.pi*t) * 0.25
         wave -= np.sin(4.2*np.pi*t) * 0.22
@@ -39,14 +35,20 @@ def synthesize():
         wave += np.sin(0.215*np.pi*t) * 0.156
         wave -= np.sin(1.1*np.pi*t) * 0.134
         wave += np.sin(0.545*np.pi*t) * 0.25
-        wave += np.sin(4*np.pi*t*t) * 0.125
-        wave -= np.sin(1*np.pi*t*t) * 0.125
-        wave += np.sin(0.123*np.pi*t*t) * 0.121
-        wave -= np.sin(0.225*np.pi*t*t) * 0.125
-        wave += np.sin(1.2*np.pi*t*t) * 0.124
-        wave -= np.sin(0.54*np.pi*t*t) * 0.138
+        wave -= np.sin(4*np.pi*t*t) * 0.125
+        wave += np.sin(1*np.pi*t*t) * 0.125
+        wave -= np.sin(0.123*np.pi*t*t) * 0.121
+        wave += np.sin(0.225*np.pi*t*t) * 0.125
+        wave -= np.sin(1.2*np.pi*t*t) * 0.124
+        wave += np.sin(0.54*np.pi*t*t) * 0.138
 
-    wave *= 0.2
+    wave_max = np.max(wave)
+    print(wave_max)
+    if len(pitch) > 4:
+        volume = 0.1 * 3 / 6
+    else:
+        volume = 0.1
+    wave = wave * volume
     pos += bufsize
 
     if len(pitch) == 0:
